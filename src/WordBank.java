4348a1fa2a768b35062dd6d39281594431951e1c
Jaan Soulier <jaansoulier@gmail.com>
2022-10-15 09:22:41 -0400

    Testing

---
+++ src/WordBank.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class WordBank
+{
+
+    public boolean isWordValid(String word)
+    {
+
+    }
+
+
+    public int getWordValue(String word)
+    {
+
+    }
+
+
+}
