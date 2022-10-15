29f938433906225bc07618f4757fd9d184c5f22c
Jaan Soulier <jaansoulier@gmail.com>
2022-10-15 10:02:03 -0400

    Added to WorkBank

--- src/WordBank.java
+++ src/WordBank.java
@@ -1,18 +1,73 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class WordBank
 {
+    private HashSet<Integer> hashedWords = new HashSet<>();
+    private HashMap<Character, Integer> letterValues = new HashMap<>();
+
+    WordBank() {
+
+        letterValues.put('E', 1);
+        letterValues.put('A', 1);
+        letterValues.put('I', 1);
+        letterValues.put('O', 1);
+        letterValues.put('N', 1);
+        letterValues.put('R', 1);
+        letterValues.put('T', 1);
+        letterValues.put('L', 1);
+        letterValues.put('S', 1);
+        letterValues.put('U', 1);
+        letterValues.put('D', 2);
+        letterValues.put('G', 2);
+        letterValues.put('B', 3);
+        letterValues.put('C', 3);
+        letterValues.put('M', 3);
+        letterValues.put('P', 3);
+        letterValues.put('F', 4);
+        letterValues.put('H', 4);
+        letterValues.put('V', 4);
+        letterValues.put('W', 4);
+        letterValues.put('Y', 4);
+        letterValues.put('K', 5);
+        letterValues.put('J', 8);
+        letterValues.put('X', 8);
+        letterValues.put('Q', 10);
+        letterValues.put('Z', 10);
+
+    }
 
     public boolean isWordValid(String word)
     {
+        if (word == null)
+        {
+            throw new NullPointerException();
+        }
 
+
+        return hashedWords.contains(word.hashCode());
     }
 
+    public int getLetterValue(Character letter)
+    {
+        return letterValues.get(letter).intValue();
+    }
 
     public int getWordValue(String word)
     {
+        if (word == null)
+        {
+            throw new NullPointerException();
+        }
 
+
+        int total = 0;
+
+        for (Character letter : word.toCharArray())
+        {
+            total += getLetterValue(letter);
+        }
+
+        return total;
     }
-
-
 }
