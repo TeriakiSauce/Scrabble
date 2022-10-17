18d7e140b71cd659bb47a113c0de105627826f80
Jaan Soulier <jaansoulier@gmail.com>
2022-10-17 16:20:07 -0400

    word bank done, just needs comments

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         WordBank bank = new WordBank("https://www.mit.edu/~ecprice/wordlist.10000");
 
-
         System.out.println(bank.isWordValid("absolute"));
         System.out.println(bank.isWordValid("bunch of garbage"));
     }
--- src/WordReader.java
+++ src/WordReader.java
@@ -1,31 +1,57 @@
+/**
+ * @author Jaan Soulier
+ * @version 1.0
+ */
+
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 
+/**
+ * 
+ */
 public class WordReader 
 {
+    /**
+     * 
+     */
+    public static final String BACKUP = "words.txt";
 
-    BufferedReader reader;
+    /**
+     * 
+     */
+    private BufferedReader reader;
 
-
-    WordReader(String path) 
+    /**
+     * 
+     * @param path
+     */
+    WordReader(String path)
     {
-
         try
         {
-
             URL url = new URL(path);
             URLConnection connection = url.openConnection();
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-        } catch (Exception e)
+        } catch (Exception net)
         {
-            // try reading from file here
+            try
+            {
+                reader = new BufferedReader(new FileReader(BACKUP));
+            } catch (Exception file)
+            {
+                throw new AssertionError();
+            }
         }
-
     }
 
+    /**
+     * 
+     * @return
+     */
     String getLine()
     {
         try
