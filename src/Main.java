d1006f79ab87d69b7eb73414f87ad0f33b59e809
TeriakiSauce <rikbeld@gmail.com>
2022-10-20 21:49:38 -0400

    Added Game Class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-
+package src;
 
 public class Main {
     public static void main(String[] args) {
-        WordBank bank = new WordBank("https://www.mit.edu/~ecprice/wordlist.10000");
 
-        System.out.println(bank.isWordValid("absolute"));
-        System.out.println(bank.isWordValid("bunch of garbage"));
-        System.out.println(bank.getWordValue("absolute"));
     }
 }
