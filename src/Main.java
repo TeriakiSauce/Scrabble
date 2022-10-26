3437894359f4d6aca8f1c2a77fe99ab740a83fbc
Jaan Soulier <jaansoulier@gmail.com>
2022-10-25 20:20:00 -0400

    Testing README screenshot

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// TODO:
+
+public class Main {
+    /**
+     * The Main method that initializes and plays a game object for testing
+     * @author Tarik Beldjehem
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+
+}
