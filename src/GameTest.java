300f5fc8a1d9cc0e4d6dcbff5d832ddf57cce373
TeriakiSauce <rikbeld@gmail.com>
2022-11-12 15:58:22 -0500

    Set up Test Classes for Model and Game
    Set up Test Classes for Model and Game

---
+++ src/GameTest.java
@@ -0,0 +1,63 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void reset() {
+    }
+
+    @Test
+    public void quit() {
+    }
+
+    @Test
+    public void placeBoard() {
+    }
+
+    @Test
+    public void placeHand() {
+    }
+
+    @Test
+    public void finish() {
+    }
+
+    @Test
+    public void pass() {
+    }
+
+    @Test
+    public void getState() {
+    }
+
+    @Test
+    public void getX() {
+    }
+
+    @Test
+    public void setX() {
+    }
+
+    @Test
+    public void getY() {
+    }
+
+    @Test
+    public void setY() {
+    }
+
+    @Test
+    public void getN() {
+    }
+
+    @Test
+    public void setN() {
+    }
+}
