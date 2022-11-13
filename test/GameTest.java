c65a31a7be951c150c394ff63777daab6df3beca
TeriakiSauce <rikbeld@gmail.com>
2022-11-13 14:48:41 -0500

    modified test classes
    modified test classes

--- test/GameTest.java
+++ test/GameTest.java
@@ -3,59 +3,75 @@ import org.junit.*;
 
 public class GameTest {
 
+    Game game;
+
     @Before
     public void setUp() throws Exception {
+        game = new Game();
     }
 
     @Test
     public void reset() {
+        game.reset();
     }
 
     @Test
     public void quit() {
+        game.quit();
     }
 
     @Test
     public void placeBoard() {
+        game.placeBoard();
     }
 
     @Test
     public void placeHand() {
+        game.placeHand();
     }
 
     @Test
     public void finish() {
+        game.finish();
     }
 
     @Test
     public void pass() {
+        game.pass();
     }
 
     @Test
     public void getState() {
+        game.getState();
     }
 
     @Test
     public void getX() {
+        game.getX();
     }
 
     @Test
     public void setX() {
+        game.setX(1);
     }
 
     @Test
     public void getY() {
+        game.getY();
     }
 
     @Test
     public void setY() {
+        game.setY(1);
     }
 
     @Test
     public void getN() {
+        game.getN();
     }
 
     @Test
     public void setN() {
+        game.setN(1);
     }
 }
---
+++ test/StateTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StateTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void reset() {
+    }
+
+    @Test
+    public void addPlayer() {
+    }
+
+    @Test
+    public void revert() {
+    }
+
+    @Test
+    public void getBag() {
+    }
+
+    @Test
+    public void getWordBank() {
+    }
+
+    @Test
+    public void getChain() {
+    }
+
+    @Test
+    public void getBoard() {
+    }
+
+    @Test
+    public void getPlayer() {
+    }
+
+    @Test
+    public void getTurn() {
+    }
+}
