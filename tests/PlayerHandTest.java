09ce5c5492712380bb4fc5b7739984aab624e7ac
TeriakiSauce <rikbeld@gmail.com>
2022-11-13 16:16:07 -0500

    Finished PlayerHand test class
    Finished PlayerHand test class

--- tests/PlayerHandTest.java
+++ tests/PlayerHandTest.java
@@ -10,27 +10,35 @@ import static org.junit.Assert.*;
  */
 public class PlayerHandTest {
 
+    PlayerHand hand;
+
     @Before
     public void setUp() throws Exception {
+        hand = new PlayerHand();
     }
 
     @Test
-    public void setLetter() {
+    public void testSetLetter() {
+        assertFalse(hand.hasLetter(1));
+        hand.setLetter(0, 'a');
+        assertTrue(hand.hasLetter(0));
+        char letter = hand.getLetter(0);
+        assertEquals(letter, 'a');
     }
 
     @Test
-    public void getLetter() {
+    public void testRemoveLetter() {
+        hand.setLetter(0, 'a');
+        assertTrue(hand.hasLetter(0));
+        hand.removeLetter(0);
+        assertFalse(hand.hasLetter(0));
     }
 
     @Test
-    public void hasLetter() {
-    }
-
-    @Test
-    public void removeLetter() {
-    }
-
-    @Test
-    public void makeCopy() {
+    public void testMakeCopy() {
+        hand.setLetter(0, 'a');
+        hand.setLetter(1, 'b');
+        PlayerHand copy = hand.makeCopy();
+        assertEquals(hand, copy);
     }
 }
