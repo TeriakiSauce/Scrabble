8ecbae57abc409c7e2d441a1ac3ccbacf89539b8
Jaan Soulier <jaansoulier@gmail.com>
2022-12-01 17:07:39 -0500

    merge should be fixed

--- tests/PlayerBotTest.java
+++ tests/PlayerBotTest.java
@@ -34,21 +34,21 @@ public class PlayerBotTest {
 
         bot.collectBoardWords();
         LinkedList words = (LinkedList) bot.getCurrentWords();
-        LetterChain answer1 = new LetterChain(game.getState());
+        LetterChain answer1 = new LetterChain(game);
         answer1.addLetter(new LetterCell(7,7,'a'));
         answer1.addLetter(new LetterCell(7,8,'s'));
         answer1.addLetter(new LetterCell(7,9,'h'));
         answer1.setIsVertical(true);
 
-        LetterChain answer2 = new LetterChain(game.getState());
+        LetterChain answer2 = new LetterChain(game);
         answer2.addLetter(new LetterCell(7,7,'a'));
         answer2.setIsVertical(false);
 
-        LetterChain answer3 = new LetterChain(game.getState());
+        LetterChain answer3 = new LetterChain(game);
         answer3.addLetter(new LetterCell(7,8,'s'));
         answer3.setIsVertical(false);
 
-        LetterChain answer4 = new LetterChain(game.getState());
+        LetterChain answer4 = new LetterChain(game);
         answer4.addLetter(new LetterCell(7,9,'h'));
         answer4.setIsVertical(false);
 
@@ -90,7 +90,7 @@ public class PlayerBotTest {
     public void testChoosePlay() {
 
         bot.calculatePossiblePoints();
-        LetterChain optimal = new LetterChain(game.getState());
+        LetterChain optimal = new LetterChain(game);
         optimal.addLetter(new LetterCell(6,9,'c'));
         optimal.addLetter(new LetterCell(8,9,'a'));
         assert(bot.choosePlay().equals(optimal));
